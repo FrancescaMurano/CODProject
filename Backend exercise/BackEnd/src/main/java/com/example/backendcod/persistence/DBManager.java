aff0cb79fee541385ad4b2beab1f969e18d29581
FrancescaMurano <mrnfnc99d60b774e@studenti.unical.it>
2022-12-02 13:12:48 +0100

    insert of db sources

---
+++ Backend exercise/BackEnd/src/main/java/com/example/backendcod/persistence/DBManager.java
@@ -0,0 +1,34 @@
+package com.example.backendcod.persistence;
+
+public class DBManager {
+
+	private static DBManager instance = null;
+	private static DBSource dataSource = null;
+	
+	static {
+		try {
+			Class.forName("org.postgresql.Driver");
+			dataSource = new DBSource("jdbc:postgresql://localhost:8080/INSERIREPROGETTO","","");
+            System.out.println("Success");
+		} 
+		catch (Exception e) {
+			System.err.println("PostgresDAOFactory.class: failed to load postgresql JDBC driver\n"+e);
+			e.printStackTrace();
+		}
+	}
+	
+	public static DBManager getInstance() {
+		if (instance == null) {
+			instance = new DBManager();
+		}
+		return instance;
+	}
+	
+	private DBManager() {}
+	
+	public static DBSource getDataSource() {
+		System.out.println("OK");
+		return dataSource;
+	}
+	
+}
