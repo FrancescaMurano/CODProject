20d4866eb97c77087363d9022bf93a02afcbe574
santino locanto <santino.locanto2015@libero.it>
2022-12-08 15:59:01 +0100

    modify

--- Backend exercise/BackEnd/src/main/java/com/example/backendcod/controller/LoginController.java
+++ Backend exercise/BackEnd/src/main/java/com/example/backendcod/controller/LoginController.java
@@ -3,18 +3,15 @@ package com.example.backendcod.controller;
 import com.example.backendcod.entities.IP;
 import com.example.backendcod.entities.LoginData;
 import com.example.backendcod.persistence.DBManager;
-import com.example.backendcod.persistence.DBSource;
 import com.example.backendcod.persistence.IPDAO;
 import com.example.backendcod.persistence.UserDAO;
 import com.example.backendcod.utils.HttpUtils;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-import java.net.http.HttpRequest;
 import java.sql.Timestamp;
 import java.sql.SQLException;
 
-import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     @PostMapping("/login")
-    public ResponseEntity<Boolean> login(@RequestBody LoginData loginData, HttpServletRequest request) {
+    public ResponseEntity<String> login(@RequestBody LoginData loginData, HttpServletRequest request) {
         DBManager.getDataSource();
         String IP_number = HttpUtils.getRequestIP(request);
         IP ip = null;
@@ -37,52 +34,48 @@ public class LoginController {
             UserDAO userDAO = new UserDAO(DBManager.getDataSource());
             IPDAO ipdao = new IPDAO(DBManager.getDataSource());
             ip = ipdao.getByIdNumber(IP_number);
-
-            if (userDAO.login(loginData)) {
-                System.out.println("Esiste carlos");
-                if (ip != null) { // è presente nella tabella
+            Long time = new Timestamp(System.currentTimeMillis()).getTime();
+            if (userDAO.login(loginData)) {// è presente nella tabella
+                if(ip!=null && (ip.getAttempts()<=3 || (time - ip.getTimeinmillis()) > 60000)){
                     ipdao.resetAttempts(IP_number, 0);
+                    return ResponseEntity.ok("Sei loggato");
                 }
-                return ResponseEntity.ok(true);
-            } else {
+                else if (ip == null){
+                    return ResponseEntity.ok("Sei loggato");
+                }
+                else{
+                    ipdao.updateAttempts(IP_number);
+                    return ResponseEntity.ok("Sei bloccato. Riprova tra 1 minuto");
+                }
+            } 
+            else {
                 if (ip != null) { // è presente nella tabella
-                    Long time = new Timestamp(System.currentTimeMillis()).getTime();
-                    Long time_difference = time - ip.getTimeinmillis();
-                    System.out.println("time_difference "+time_difference);
                     if (ip.getAttempts() > 3) {
                         // numero di tentativi > 3 ma non è passato 1 minuto
-                        if (time_difference < 60000) {
-                             
-                            System.out.println("Login non corretto e sei bloccato");
+                        if ((time - ip.getTimeinmillis()) < 60000) {
                             ipdao.updateAttempts(IP_number);
+                            return ResponseEntity.ok("Sei bloccato. Riprova tra 1 minuto");
                         } else {
                             // numero di tentativi > 3 ma è passato 1 minuto
                             ipdao.resetAttempts(IP_number, 1);
-                            System.out.println("Login non corretto ma non sei più bloccato");
                         }
                     } else {
                         // numero di tentativi <=3 ma è passato 1 minuto
-                        if (time_difference > 60000) {
-                            System.out.println("Login non corretto, è passato un minuto");
-                            ipdao.resetAttempts(IP_number, 1);
+                        if ((time - ip.getTimeinmillis()) > 60000) {
+                            ipdao.resetAttempts(IP_number, 1);    
                         }
                         // numero di tentativi <=3 ma NON è passato 1 minuto
                         else {
-                            System.out.println("Login non corretto, NON è passato un minuto");
                             ipdao.updateAttempts(IP_number);
                         }
-
                     }
-                } else {
+                } 
+                else 
                     ipdao.insertIP(new IP(IP_number));
-                }
-                
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
-        return ResponseEntity.ok(true);
-    }
-
+        return ResponseEntity.ok("Username o password non validi");
+    } 
 }
--- Backend exercise/BackEnd/src/main/java/com/example/backendcod/main.java
+++ Backend exercise/BackEnd/src/main/java/com/example/backendcod/main.java
@@ -1,9 +1,7 @@
 package com.example.backendcod;
 
-import java.sql.Timestamp;
 import java.sql.SQLException;
 
-import org.json.*;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import com.example.backendcod.controller.LoginController;
@@ -12,18 +10,16 @@ import com.example.backendcod.persistence.DBManager;
 import com.example.backendcod.persistence.UserDAO;
 
 public class main {
-    public static void main(String[] args) throws InterruptedException{
+   /* public static void main(String[] args) throws InterruptedException{
         try {
             // JSONObject jsonObject = new JSONObject();
 
             // jsonObject.put("username", "carlo");
             // jsonObject.put("password", "montoya");
-            LoginData login = new LoginData();
-            UserDAO userDAO = new UserDAO(DBManager.getDataSource());
-            login.setUsername("carlos");
-            login.setPassword("ciao");
-            LoginController controller = new LoginController();
-            controller.login(login, null);
+           // LoginData login = new LoginData();
+           // UserDAO userDAO = new UserDAO(DBManager.getDataSource());
+           // String pass = BCrypt.hashpw("rockyou", BCrypt.gensalt(12));
+           // System.out.println(pass);
             // userDAO.login(login);
             // userDAO.login(login);
             // userDAO.login(login);
@@ -37,5 +33,5 @@ public class main {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 }
